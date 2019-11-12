974ea8fa8c00d4fb4a600a6258162e6274795466
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-11-12 02:55:39 +0100

    Update Serial.py

--- 100 - Logiciels Buggy/Serial.py
+++ 100 - Logiciels Buggy/Serial.py
@@ -2,14 +2,15 @@ import time
 import serial
 
 
-port = serial.Serial("/dev/ttyS0",baudrate=115200,timeout=3.0)
+#port = serial.Serial("/dev/ttyS0",baudrate=115200,timeout=3.0)
+port = serial.Serial("/dev/ttyUSB0",baudrate=115200,timeout=1.0)
 
 steering = 128.0
 throttle = 128.0
 mode = 0
 
 while True:
-	time.sleep(1)
+	time.sleep(0.1)
 	message_out =  "{:d};".format(int(steering)).encode('ascii') + "{:d};".format(int(throttle)).encode('ascii') + "{:d}\r\n".format(mode).encode('ascii')
 	#print(">>>" + str(message_out) )
 	port.write("{:d};".format(int(steering)).encode('ascii') + "{:d};".format(int(throttle)).encode('ascii') + "{:d}\r\n".format(mode).encode('ascii'))
