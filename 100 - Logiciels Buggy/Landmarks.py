27fbaafe67cb0f39b1f89a451a0a288ab0e48503
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-11-14 20:49:13 +0100

    buggy

--- 100 - Logiciels Buggy/Landmarks.py
+++ 100 - Logiciels Buggy/Landmarks.py
@@ -4,13 +4,76 @@ import math
 # hyp : -135,+135, step 1°
 # tune anchor radius
 # tune max distance
+
+
+'''
+def is_clusterized(a1,  d1,  a2,  d2):
+    cluster_distance_criteria = 150
+    # if a measure is missing, cluster is ended
+    angle_criteria = 0.5
+    
+    if abs(a1 - a2) > angle_criteria:
+        return False
+    
+    if abs(d1 - )
+
+    return abs(dista)
+'''
+
+# return a list of clusters
 def localize_landmarks(lidar_distance):
+    #initiate variables
+    last_distance = 0
+    distance = 10000
+    cluster_size = 0
+    cluster_start_angle = 0
+    landmarks = []
+    cluster = []
+    #minimum size of cluster
+    min_cluster_size = 10
+    max_cluster_size = 40
+    #distance between two points for clustering
+    clustering_distance = 10
+    
+       
+        
+    for i in range(lidar_distance.shape[0]):
+        angle = math.degrees(lidar_distance[i,0])
+        last_distance = distance
+        distance = lidar_distance[i,1]
+        
+        #is a cluster detected ? 
+        if abs(last_distance - distance) < clustering_distance:
+            if cluster_size > 0:
+                    #cluster is continued
+                    cluster_size = cluster_size + 1
+                                
+            else:
+                #start cluster
+                cluster_size = 2
+                cluster_start_angle = angle
+        
+        else:            
+            if cluster_size > 0:
+                #end of cluster
+                # is it big enough ? 
+                if cluster_size > min_cluster_size and cluster_size < max_cluster_size:
+                    # add cluster to landmark list
+                    #landmarks.append( ((angle + cluster_start_angle)/2, distance/1000))
+                    
+                    landmarks.append(cluster)
+                cluster_size = 0
+                
+    return landmarks
+        
+
+def localize_landmarks_old(lidar_distance):
 	# process LIDAR point clound to find landmarks
 	landmarks = []
 	anchor_radius = 0.05 #m
 	threshold_max_distance = 9.5 #m
 	# find cluster
-	cluster_radius = 0.4 #m (at least 1m between landmarks)
+	cluster_radius = 0.5 #m (at least 1m between landmarks)
 	current_cluster_min_distance = 0.0 #m
 	current_cluster_min_angle = 0.0 # deg
 	current_cluster_begin_angle = 0.0 # deg
@@ -31,7 +94,7 @@ def localize_landmarks(lidar_distance):
 
 		elif in_cluster and (distance >= threshold_max_distance): # end of current cluster
 			# regsiter current cluster
-			if( current_cluster_min_distance*math.sin(math.radians(current_cluster_end_angle-current_cluster_begin_angle)) <= 2.0*anchor_radius):
+			if( current_cluster_min_distance*math.sin(math.radians(current_cluster_end_angle-current_cluster_begin_angle)) <= 10.0*anchor_radius):
 				landmarks.append( (current_cluster_min_angle, current_cluster_min_distance+anchor_radius ))
 			in_cluster = False
 
@@ -45,7 +108,7 @@ def localize_landmarks(lidar_distance):
 
 		elif in_cluster and (distance < threshold_max_distance) and (abs(distance-current_cluster_min_distance)>cluster_radius) : # end of current cluster and new cluster detection
 			# regsiter current cluster
-			if( current_cluster_min_distance*math.sin(math.radians(current_cluster_end_angle-current_cluster_begin_angle)) <= 2.0*anchor_radius):
+			if( current_cluster_min_distance*math.sin(math.radians(current_cluster_end_angle-current_cluster_begin_angle)) <= 10.0*anchor_radius):
 				landmarks.append( (current_cluster_min_angle, current_cluster_min_distance+anchor_radius ))
 			in_cluster = False
 			# start new cluster
--- 100 - Logiciels Buggy/Serial.py
+++ 100 - Logiciels Buggy/Serial.py
@@ -1,19 +1,23 @@
-import time
-import serial
+from STM32_proxy import *
+
+# #port = serial.Serial("/dev/ttyS0",baudrate=115200,timeout=3.0)
+# port = serial.Serial("/dev/ttyUSB0",baudrate=115200,timeout=1.0)
+
+# steering = 128.0
+# throttle = 128.0
+# mode = 0
+
+# while True:
+# 	message_in = port.readline()
+# 	print(message_in)
+# 	#message_out =  "{:d};".format(int(steering)).encode('ascii') + "{:d};".format(int(throttle)).encode('ascii') + "{:d}\r\n".format(mode).encode('ascii')
+# 	#port.write("{:d};".format(int(steering)).encode('ascii') + "{:d};".format(int(throttle)).encode('ascii') + "{:d}\r\n".format(mode).encode('ascii'))
 
 
-#port = serial.Serial("/dev/ttyS0",baudrate=115200,timeout=3.0)
-port = serial.Serial("/dev/ttyUSB0",baudrate=115200,timeout=1.0)
 
-steering = 128.0
-throttle = 128.0
-mode = 0
+proxy =  stm32_proxy("/dev/ttyUSB0")
+
 
 while True:
-	time.sleep(0.1)
-	message_out =  "{:d};".format(int(steering)).encode('ascii') + "{:d};".format(int(throttle)).encode('ascii') + "{:d}\r\n".format(mode).encode('ascii')
-	#print(">>>" + str(message_out) )
-	port.write("{:d};".format(int(steering)).encode('ascii') + "{:d};".format(int(throttle)).encode('ascii') + "{:d}\r\n".format(mode).encode('ascii'))
-	message_in = port.readline()
-	print(message_in)
-
+	proxy.receive()
+	
