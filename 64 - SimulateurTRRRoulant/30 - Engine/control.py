6d6ce7128ebe571df1542c269f33e40290e6c88f
pat92fr <48148915+pat92fr@users.noreply.github.com>
2020-02-27 14:09:45 +0100

    simu TRR

--- 64 - SimulateurTRRRoulant/30 - Engine/control.py
+++ 64 - SimulateurTRRRoulant/30 - Engine/control.py
@@ -6,12 +6,12 @@ class robot_controller:
 	def __init__(self):
 
 		# speed controller settings
-		self.min_speed_ms = 2.0 # 1.0 m/s
-		self.cornering_speed = 3.0
-		self.max_speed_ms = 10.0 # 10 m/s
+		self.min_speed_ms = 2.5 # 1.0 m/s
+		self.cornering_speed = 4.5
+		self.max_speed_ms = 12.0 # 10 m/s
 
-		self.acceleration = 0.1 # m/s per 1/60eme
-		self.deceleration = 0.5 # m/s per 1/60eme
+		self.acceleration = 0.15 # m/s per 1/60eme
+		self.deceleration = 0.6 # m/s per 1/60eme
 
 		self.pid_speed = my_controller.pid(kp=0.3, ki=0.0, kd=0.1, integral_max=1000, output_max=1.0, alpha=0.5) 
 		self.pid_speed_kff = 0.0 # feed forward apart from speed PID
@@ -25,8 +25,8 @@ class robot_controller:
 		self.actual_speed_error_ms = 0.0 # m/s
 
 		# lidar steering controller settings
-		self.pid_wall_following = my_controller.pid(kp=0.6, ki=0.0, kd=8.0, integral_max=1000, output_max=1.0, alpha=0.2) 
-		self.lidar_direction_k_speed = 0.2
+		self.pid_wall_following = my_controller.pid(kp=1.0, ki=0.0, kd=20.0, integral_max=1000, output_max=1.0, alpha=0.2) 
+		self.lidar_direction_k_speed = 0.4
 		self.lidar_maximum_distance = 2.0
 
 		# lidar steering controller state
@@ -37,8 +37,8 @@ class robot_controller:
 		self.ratio_ai = 1.0
 
 		# steering settings
-		self.ration_ai_x1 = 0.2 #
-		self.ration_ai_x2 = 0.4 #
+		self.ration_ai_x1 = 0.05 #
+		self.ration_ai_x2 = 0.3 #
 
 		print("Done.")
 
@@ -55,6 +55,38 @@ class robot_controller:
 			return self.max_speed_ms
 		else:
 			return self.cornering_speed
+
+	# position strategy 
+	# return left_distance or 0, right distance or 0,
+	# 0.3,0.0 means "maintain distance with left rail at about 30cm"
+	# 0.0,0.0 means "maintain distance with left and right rails (centered)"
+	def position_from_distance(self, distance):
+		distance = distance % 110.0
+
+		if distance >= 0.0 and distance < 5.0: # ligne droite apres arche
+			return 0.0, 0.0
+
+		elif distance >= 5.0 and distance < 15.0: # premier virage
+			return 0.8, 0.0 # 0.6 0.0
+
+		elif distance >= 15.0 and distance < 26.0: # ligne droite avant chicane
+			return 0.0, 0.0
+
+		elif distance >= 26.0 and distance < 45.0: # chicane
+			return 0.0, 0.0		
+
+		elif distance >= 45.0 and distance < 56.0: # ligne droite apres chicane
+			return 0.0, 0.0
+
+		elif distance >= 56.0 and distance < 65.0: # dernier virage
+			return 0.8, 0.0 # 0.6 0.0
+
+		elif distance > 65.0 and distance < 120.0: # grande ligne droite
+			return 0.0, 0.0
+		
+		else:
+			return 0.0, 0.0
+
 
 	def process(
 		self,				
@@ -75,11 +107,17 @@ class robot_controller:
 
 		# speed controller (stage 1)
 		self.target_speed_ms_ref = self.max_speed_from_distance(total_distance)
+		distance_to_left, distance_to_right = self.position_from_distance(total_distance)
 
 		# wall following PID controller
 		lidar_distance_droit = constraint(lidar_distance_droit,0, self.lidar_maximum_distance)
 		lidar_distance_gauche = constraint(lidar_distance_gauche,0, self.lidar_maximum_distance)
-		self.actual_lidar_direction_error = -constraint(lidar_distance_droit - lidar_distance_gauche, -self.lidar_maximum_distance, self.lidar_maximum_distance)/self.lidar_maximum_distance
+		if distance_to_left == 0.0 and distance_to_right == 0.0:
+				self.actual_lidar_direction_error = -constraint(lidar_distance_droit - lidar_distance_gauche, -self.lidar_maximum_distance, self.lidar_maximum_distance)/self.lidar_maximum_distance
+		elif distance_to_left > 0.0:
+				self.actual_lidar_direction_error = -constraint(distance_to_left - lidar_distance_gauche, -0.5, 0.5) / 0.5
+		elif distance_to_right > 0.0:
+				self.actual_lidar_direction_error = -constraint(lidar_distance_droit - distance_to_right, -0.5, 0.5) / 0.5
 		self.pid_wall = self.pid_wall_following.compute(self.actual_lidar_direction_error)
 
 		# blending PID
